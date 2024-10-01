b258f3b65736a242883df2b53c5f98f916dddf9c
Irfan1286 <tihanben@gmail.com>
2024-10-01 10:38:12 +0600

    Addition of better comments & Changed the link page from gainers to most-active

--- Yahoo-Stocks-Scrape.py
+++ Yahoo-Stocks-Scrape.py
@@ -1,57 +1,64 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import csv
 
 from selenium import webdriver
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 # Gets the Entire Table
 def getTable(url):
-    
+
     soup = getSoup(url)
 
-    # Check for pagination existence
+    # When there is no Pagination!
     pagination = soup.find('div', class_='select-dropdown yf-1tdhqb1')
 
-    if pagination != None:
+    if pagination == None:
+        dfFrame = getRows(soup)
+        dfFrame.to_csv('Example-output.csv', index=False)
         
-        dfList = []
-        driver = getDriver(url)
+
+    # For 25+ rows of data!
+    else:
+        # Generates Soup from drivers html! 
+        driver = getDriver(url)     
         soup = BeautifulSoup(driver.page_source, 'lxml')
 
+        dfList = []
+
+        # looping through modified URL for other sets of 25 rows of data
         while True:
             
+            # Get current set of Rows & adds DataFrame to list!
             dfList.append(getRows(soup))
-            
 
+            # Getting the total rows & Starting Row in table
             total_pg = soup.find('div', class_='total yf-1tdhqb1').get_text()   #type: ignore
             pg_info = total_pg.split('of')
 
             total = int(pg_info[1])
             endRow = int(pg_info[0].split('-')[1])
 
+            # Combine DataFrames in list to generate CSV file, & Break the loop!
             if endRow >= total:
 
                 dfCombined = pd.concat(dfList, ignore_index=True)
                 dfCombined.to_csv('Example-output.csv', index=False)
 
                 break
+            
+            # Modifies URL to get next set of data/25-Rows
+            else:
+                new_url = f'{url}?start={endRow}&count=25'
+                print(new_url)
 
-            new_url = f'{url}?start={endRow}&count=25'
-            print(new_url)
+                driver.get(new_url)
 
-            driver.get(new_url)
-
-            soup = BeautifulSoup(driver.page_source)
+                soup = BeautifulSoup(driver.page_source)
         
         print(dfList)
 
-    else:
-        pdFrame = getRows(soup)
-        pdFrame.to_csv('Example-output.csv', index=False)
-
-# Returns a BS4 result element
+# Returns the Soup!
 def getSoup(url):
     request = requests.get(url)
     html = request.text
@@ -61,10 +68,11 @@ def getSoup(url):
 
 # Returns a Soup from driver
 def getDriver(url):
-    options = webdriver.EdgeOptions()
 
-    options.add_argument("--headless")  # Run in headless mode (no browser UI)
-    options.add_argument("--disable-gpu")  # Disable GPU rendering for increased performance
+    # Setting Options to run Browser Without UI & disabling GPU for increased Performance!
+    options = webdriver.EdgeOptions()
+    options.add_argument("--headless")  
+    options.add_argument("--disable-gpu") 
 
     service = webdriver.EdgeService(EdgeChromiumDriverManager().install())
     driver = webdriver.Edge(service=service, options=options)
@@ -73,43 +81,44 @@ def getDriver(url):
     
     return driver
 
-# Returns a PandaFrame
+# Returns a DataFrame
 def getRows(soup):
 
-    # finds the table tag
+    # Finds the table & Creates a list for rows
     table = soup.find('tbody')
     rows = table.find_all('tr') 
 
-    # symbol, name, price, change, percentChange, Vol, avgVol, marketCap, PEratio
+    # symbol, name, price, change, percentChange, Vol, avgVol, marketCap, PEratio, 52 Week Change %
     columns = [[] for i in range(10)]
 
 
-    for cell in rows:
-        data = cell.find_all('td')
+    for r in rows:
+        # Gets all the Cells per row
+        cellData = r.find_all('td')
         
+        # Retrieves text & appends them to the respective Columns
         for i in range(2, len(columns)):
-            columns[i].append(data[i-1].get_text())
+            columns[i].append(cellData[i-1].get_text())
 
-        # adding symbol & name
-        columns[0].append(data[0].find('span', class_='symbol yf-1jpysdn').get_text())
-        columns[1].append(data[0].find('span', class_='yf-1jpysdn longName').get_text())
+        # Symbol & Name are on same CellData[0]
+        columns[0].append(cellData[0].find('span', class_='symbol yf-1jpysdn').get_text())
+        columns[1].append(cellData[0].find('span', class_='yf-1jpysdn longName').get_text())
 
-    columns[2] = [price.split(' ')[1] for price in columns[2]] 
 
+    columns[2] = [price.split(' ')[1] for price in columns[2]]      # This line removes Unneccessary data that appears for this column
+
+    # Creates a 2D Dictionary with all these Columns
     tbColumns = ['Symbol', 'Name', 'Price', 'Change', '%Change', 'Volume', 
                  'AvgVolume', 'MarketCap', 'P/E Ratio (TTM)', '52 Wk Change %']
-    
     tb_dict = {}
 
     for i, column in enumerate(tbColumns):
         tb_dict[column] = columns[i]
 
-    pdFrame = pd.DataFrame(tb_dict)
+    dfFrame = pd.DataFrame(tb_dict)
     
-    return pdFrame
-
-
+    return dfFrame
 
 
 if __name__ == '__main__':
-    getTable('https://finance.yahoo.com/markets/stocks/gainers/')
+    getTable('https://finance.yahoo.com/markets/stocks/most-active/')
